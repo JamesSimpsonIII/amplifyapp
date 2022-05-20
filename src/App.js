23508f26bae4d574bc632e2b333ad01e6023446b
James Simpson III <81001571+JamesSimpsonIII@users.noreply.github.com>
2022-05-20 12:39:05 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>This Is A New Deployment</h1>
+        <h1>This is A New Deployment</h1>
       </header>
     </div>
   );
